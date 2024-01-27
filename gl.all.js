17541b9c98d1dc0fb7f04365ccb0de986399246d
mapqq <156518960+mapqq@users.noreply.github.com>
2024-01-27 13:45:49 +0800

    Create gl.all.js

---
+++ gl.all.js
@@ -0,0 +1,20 @@
+const currentLocationHref = window.location.href;
+const targetEnv = ['e5test', 'e5test2', 'ehospital2', 'e5'];
+const targetHos = ['ccscrbyyadmin', 'syeswyyadmin'];
+
+const removeCache = () => {
+  const hid = sessionStorage.getItem('hospitalID') || sessionStorage.getItem('hospital_id') || localStorage.getItem('hospitalID') || localStorage.getItem('hospital_id');
+  const isTarget = targetEnv.some(env => currentLocationHref.includes(env)) && targetHos.includes(hid);
+  if (isTarget) {
+    localStorage.clear();
+    sessionStorage.clear();
+  }
+};
+
+window.onload = function () {
+  removeCache();
+}
+
+setTimeout(() => {
+  removeCache();
+}, 5000);
